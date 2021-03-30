9af2517f6eff3f8aaccd50b47c7f1fe4492f5e36
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-03-30 17:15:25 +0600

    added code

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('working');
+})
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.af2ol.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
